1ab4ea0675627bba69887f4a33123f3e24a65775
EnricoScariot <scariotenrico@gmail.com>
2017-03-19 19:48:40 +0100

    tolto trigger da drw e untap phase

--- src/magic2/DrawPhase.java
+++ src/magic2/DrawPhase.java
@@ -26,36 +26,7 @@ public class DrawPhase implements Phase{
      
          // l'avversario risponde
         
-        int current1,i; 
-        do{
-               current1=-1;
-         while(current1 != 0 && current1!= 1){
-                    System.out.println(enemy.getName()+ " did you want to play an instant? (0/1)");
-                    current1 = sc.nextInt();
-                    }  
-                 if(current1==1){
-                      
-                         System.out.println("Which card do you want to play?(0/"+(enemy.getHand().size()-1)+")");
-
-                      i = sc.nextInt();
-                      enemy.responde(i);
-                      
-                     
-                    }  
-         current1=-1;               
-         while(current1 != 0 && current1!= 1){
-                    System.out.println(current.getName()+ " did you want to play an instant? (0/1)");
-                    current1 = sc.nextInt();
-                    }  
-                 if(current1==1){
-                      
-                         System.out.println("Which card do you want to play?(0/"+(current.getHand().size()-1)+")");
-
-                      i = sc.nextInt();
-                      current.responde(i);
-                     
-                 }
-        }while(current1!=0 && current.getHand().size()>0&&enemy.getHand().size()>0);
+      
     }
     
     
